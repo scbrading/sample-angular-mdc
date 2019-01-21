640ededa1130edf00f52b5318076d9c555cb82a0
Clark Brading <scbrading@gmail.com>
2019-01-21 08:39:00 -0500

    Forms component added.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,12 +19,12 @@ export class AppComponent implements OnInit {
   @ViewChild('appDrawer') appDrawer: MdcDrawer;
 
   navigationLinks = [
-    {name: 'Home', route: 'home', icon: '../assets/Icon%20-%20Nav%20Drawer%20-%20Home.svg'},
-    {name: 'General', route: 'general-settings', icon: '../assets/Icon%20-%20Nav%20Drawer%20-%20General%20Settings.svg'},
-    {name: 'Roles', route: 'roles', icon: '../assets/Icon%20-%20Nav%20Drawer%20-%20Roles.svg'},
-    {name: 'Users', route: 'users', icon: '../assets/Icon%20-%20Nav%20Drawer%20-%20Users.svg'},
-    {name: 'Forms', route: 'forms', icon: '../assets/Icon%20-%20Nav%20Drawer%20-%20Forms.svg'},
-    {name: 'Episodes', route: 'episodes', icon: '../assets/Icon%20-%20Nav%20Drawer%20-%20Episodes.svg'}
+    {name: 'Home', route: 'home', icon: '../assets/Icon%20-%2032x24%20-%20Home.svg'},
+    {name: 'General', route: 'general-settings', icon: '../assets/Icon%20-%2032x24%20-%20General%20Settings.svg'},
+    {name: 'Roles', route: 'roles', icon: '../assets/Icon%20-%2032x24%20-%20Roles.svg'},
+    {name: 'Users', route: 'users', icon: '../assets/Icon%20-%2032x24%20-%20Users.svg'},
+    {name: 'Forms', route: 'forms', icon: '../assets/Icon%20-%2032x24%20-%20Forms.svg'},
+    {name: 'Episodes', route: 'episodes', icon: '../assets/Icon%20-%2032x24%20-%20Episodes.svg'}
   ];
 
   isScreenSmall(): boolean {
@@ -33,12 +33,13 @@ export class AppComponent implements OnInit {
 
   ngOnInit(): void {
     this.matcher = matchMedia(`(max-width: ${SMALL_WIDTH_BREAKPOINT}px)`);
-    console.log(this.matcher);
   }
 
-  onDrawerSelect(route: string) {
+  onDrawerSelect(route: string, param) {
     this._router.navigate([route]);
 
     this.appDrawer.open = false;
+    console.log(this.appDrawer);
+    console.log(param);
   }
 }
